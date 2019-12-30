9e3c1071deacd8dbf5a4e7633b11b688ab49225f
Lúcás Meier <cronokirby@gmail.com>
2019-12-30 07:38:11 +0100

    Add basic lexing
    This lexing is sufficient to lex the first test program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let program = include_str!("../test-programs/1.iku");
+    for t in lexer::lex(program.chars()) {
+        println!("{:?}", t);
+    }
 }
