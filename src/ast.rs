7960b8eb73eebbe473a868d2ea1b8f9da8a98193
Lúcás Meier <cronokirby@gmail.com>
2020-01-01 13:59:44 +0100

    Add negative litterals

--- src/ast.rs
+++ src/ast.rs
@@ -21,4 +21,12 @@ mod test {
         let ast = AST::FuncMain(Expr::Print(Box::new(Expr::I32(2))));
         assert_eq!(res, Ok(ast));
     }
+
+    #[test]
+    fn test_2() {
+        let prog = include_str!("../test-programs/2.iku");
+        let res = ASTParser::new().parse(prog);
+        let ast = AST::FuncMain(Expr::Print(Box::new(Expr::I32(-2))));
+        assert_eq!(res, Ok(ast));
+    }
 }
