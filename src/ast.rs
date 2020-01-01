ed38f34a75e4ee31f99d3de23931861202632676
Lúcás Meier <cronokirby@gmail.com>
2020-01-01 13:50:06 +0100

    Add a test for the first test program

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,24 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Expr {
     Print(Box<Expr>),
-    I32(i32)
+    I32(i32),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum AST {
-    FuncMain(Expr)
+    FuncMain(Expr),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::parse_ast::ASTParser;
+
+    #[test]
+    fn test_1() {
+        let prog = include_str!("../test-programs/1.iku");
+        let res = ASTParser::new().parse(prog);
+        let ast = AST::FuncMain(Expr::Print(Box::new(Expr::I32(2))));
+        assert_eq!(res, Ok(ast));
+    }
 }
