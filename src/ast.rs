7a6a15861656e196d8cb1ee425872669b02ac71d
Lúcás Meier <cronokirby@gmail.com>
2020-01-01 14:43:13 +0100

    Parse string litterals

--- src/ast.rs
+++ src/ast.rs
@@ -2,11 +2,43 @@
 pub enum Expr {
     Print(Box<Expr>),
     I32(i32),
+    Str(String),
 }
 
 #[derive(Debug, PartialEq)]
 pub enum AST {
     FuncMain(Expr),
+}
+
+/// Handle escape sequences
+#[allow(dead_code)]
+pub fn process_string_litteral(input: &str) -> String {
+    println!("{:?}", input);
+    let mut acc = String::new();
+    let mut escaping = false;
+    for c in input.chars() {
+        let was_escaping = escaping;
+        match (escaping, c) {
+            // This will only be the first and last
+            (_, '"') => {}
+            (true, 'n') => acc.push_str("\n"),
+            (true, '\\') => acc.push('\\'),
+            (true, 'r') => acc.push('\r'),
+            (true, 't') => acc.push('\t'),
+            (true, '0') => acc.push('\0'),
+            (true, c) => {
+                acc.push('\\');
+                acc.push(c)
+            }
+            (false, '\\') => escaping = true,
+            (false, c) => acc.push(c),
+        }
+        if was_escaping {
+            escaping = false;
+        }
+    }
+    println!("{:?}", acc);
+    acc
 }
 
 #[cfg(test)]
@@ -27,6 +59,15 @@ mod test {
         let prog = include_str!("../test-programs/2.iku");
         let res = ASTParser::new().parse(prog);
         let ast = AST::FuncMain(Expr::Print(Box::new(Expr::I32(-2))));
+        assert_eq!(res, Ok(ast));
+    }
+
+    #[test]
+    fn test_3() {
+        let prog = include_str!("../test-programs/3.iku");
+        let res = ASTParser::new().parse(prog);
+        let litt = String::from("\n\t\0\r\\今日はhello");
+        let ast = AST::FuncMain(Expr::Print(Box::new(Expr::Str(litt))));
         assert_eq!(res, Ok(ast));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,4 @@ lalrpop_mod!(pub parse_ast);
 mod ast;
 
 fn main() {
-    let program = include_str!("../test-programs/1.iku");
-    let res = parse_ast::ASTParser::new().parse(program);
-    println!("{:?}", res);
 }
